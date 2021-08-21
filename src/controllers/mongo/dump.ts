fc9ad7e376c613166b6f4e9680c288a749816ba6
RUPAK <58807337+RUPAAK@users.noreply.github.com>
2021-08-21 13:32:29 +0545

    canges

--- src/controllers/mongo/dump.ts
+++ src/controllers/mongo/dump.ts
@@ -49,8 +49,12 @@ const createDump= async(req: Request, res: Response)=>{
     // })
 
     child.on('exit', async(code: number, signal:  NodeJS.Signals)=>{
-        if(code) throw new BadRequestError(`Process exit with code: ${code}`)
-        else if(signal)throw new BadRequestError(`Process killed with signal: ${signal}`)
+        if(code){
+            res.send('Process exist')
+        }
+        else if(signal){
+            res.send('Process exists')
+        }
         else{
             console.log('Backup successfull')
 
