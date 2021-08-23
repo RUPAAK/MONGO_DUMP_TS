f720522f000dcd461ec00f34afb34a8a9f25e2ab
RUPAK <58807337+RUPAAK@users.noreply.github.com>
2021-08-23 22:59:27 +0545

    changes

--- src/controllers/mongo/dump.ts
+++ src/controllers/mongo/dump.ts
@@ -40,94 +40,96 @@ const createDump= async(req: Request, res: Response)=>{
         // console.log('hi')
         await axios.post(`${baseUrl}/logger`, {id, message: "Empty Field", data: '', state: State.Failed})
         res.end()
-    }
-    const child= spawn('mongodump', [
-        '--gzip',
-        `--uri`, `${url}`,
-        '--forceTableScan'
-    ])
-
-    child.stderr.on('data', async(data)=>{
-        console.log('stdout:', Buffer.from(data).toString())
-        await axios.post(`${baseUrl}/logger`, {id, message: Buffer.from(data).toString(), data: '', state: State.Pending})
-        res.end()
-    })
-
-    child.on('exit', async(code: number, signal:  NodeJS.Signals)=>{
-        if(code){
-            try {
-                await axios.post(`${baseUrl}/logger`, {id, message: `Backup Failed with code: ${code}`, data: '', state: State.Failed})
-                res.end()
-            } catch (error) {
-                await axios.post(`${baseUrl}/logger`, {id, message: error.message, data: '', state: State.Failed})
-            }
-        }
-        else if(signal){
-            try {
-                await axios.post(`${baseUrl}/logger`, {id, message: `Backup Failed with signal: ${signal}`, data: '', state: State.Failed})
-                res.end()
-            } catch (error) {
-                await axios.post(`${baseUrl}/logger`, {id, message: error.message, data: '', state: State.Failed})
-            }
-        }
-        else{
-            try {
-                await axios.post(`${baseUrl}/logger`, {id, message: "Backup successfull", data: '', state: State.Success_Pending})
-                if(!fs.existsSync('restore')){
-                    fs.mkdirSync('restore')
+    }else{
+        const child= spawn('mongodump', [
+            '--gzip',
+            `--uri`, `${url}`,
+            '--forceTableScan'
+        ])
+    
+        child.stderr.on('data', async(data)=>{
+            console.log('stdout:', Buffer.from(data).toString())
+            await axios.post(`${baseUrl}/logger`, {id, message: Buffer.from(data).toString(), data: '', state: State.Pending})
+            res.end()
+        })
+    
+        child.on('exit', async(code: number, signal:  NodeJS.Signals)=>{
+            if(code){
+                try {
+                    await axios.post(`${baseUrl}/logger`, {id, message: `Backup Failed with code: ${code}`, data: '', state: State.Failed})
+                    res.end()
+                } catch (error) {
+                    await axios.post(`${baseUrl}/logger`, {id, message: error.message, data: '', state: State.Failed})
                 }
-                const output = await fs.createWriteStream('restore/dump.zip');
-                const archive = archiver('zip', {
-                    zlib: {level: 9}
-                }); 
-    
-                archive.on('error', function(err: Error){
-                    throw new BadRequestError('Failed to create Zip folder')
-                });
-                
-                await archive.pipe(output);
-                
-                // append files from a sub-directory, putting its contents at the root of archive
-                await archive.directory('./dump', false);
-    
-                await archive.finalize()
-    
-                const zipPath= path.resolve('./restore/dump.zip')
-                const zipFile= fs.readFile(zipPath, async(err: any, data: Buffer)=>{
-                    if(err){
-                        await axios.post(`${baseUrl}/logger`, {id, message: err.message, data: '', state: State.Failed})
-                        res.end()
-                    }
-                    if(data){
-                        
-                        const params: Params={
-                            Bucket: process.env.AWS_S3_BUCKET_NAME!,
-                            Key: `${uuidv4()}${path.extname(zipPath)}`,
-                            Body: data
-                        }
-                        s3.upload(params, async function(s3Err: Error, aws: any){
-                            if(s3Err){
-                                await axios.post(`${baseUrl}/logger`, {id, message: `${s3Err.message}`, data: '', state: State.Failed})
-                                res.end()
-                            }
-                            if(aws){
-                                await axios.post(`${baseUrl}/logger`, {id, message: "Backup successfull", data: aws.Location, state: State.Success})
-                            fs.rm('dump', {recursive: true}, ()=>{
-                                fs.rm('restore', {recursive: true}, ()=>{
-                                    res.end()
-                                })
-                            })
-                            }
-                        })
-                    }
-                })
-            } catch (e) {
-                await axios.post(`${baseUrl}/logger`, {id, message: e.message, data: '', state: State.Failed})
-                res.end()
             }
+            else if(signal){
+                try {
+                    await axios.post(`${baseUrl}/logger`, {id, message: `Backup Failed with signal: ${signal}`, data: '', state: State.Failed})
+                    res.end()
+                } catch (error) {
+                    await axios.post(`${baseUrl}/logger`, {id, message: error.message, data: '', state: State.Failed})
+                }
+            }
+            else{
+                try {
+                    await axios.post(`${baseUrl}/logger`, {id, message: "Backup successfull", data: '', state: State.Success_Pending})
+                    if(!fs.existsSync('restore')){
+                        fs.mkdirSync('restore')
+                    }
+                    const output = await fs.createWriteStream('restore/dump.zip');
+                    const archive = archiver('zip', {
+                        zlib: {level: 9}
+                    }); 
+        
+                    archive.on('error', function(err: Error){
+                        throw new BadRequestError('Failed to create Zip folder')
+                    });
+                    
+                    await archive.pipe(output);
+                    
+                    // append files from a sub-directory, putting its contents at the root of archive
+                    await archive.directory('./dump', false);
+        
+                    await archive.finalize()
+        
+                    const zipPath= path.resolve('./restore/dump.zip')
+                    const zipFile= fs.readFile(zipPath, async(err: any, data: Buffer)=>{
+                        if(err){
+                            await axios.post(`${baseUrl}/logger`, {id, message: err.message, data: '', state: State.Failed})
+                            res.end()
+                        }
+                        if(data){
+                            
+                            const params: Params={
+                                Bucket: process.env.AWS_S3_BUCKET_NAME!,
+                                Key: `${uuidv4()}${path.extname(zipPath)}`,
+                                Body: data
+                            }
+                            s3.upload(params, async function(s3Err: Error, aws: any){
+                                if(s3Err){
+                                    await axios.post(`${baseUrl}/logger`, {id, message: `${s3Err.message}`, data: '', state: State.Failed})
+                                    res.end()
+                                }
+                                if(aws){
+                                    await axios.post(`${baseUrl}/logger`, {id, message: "Backup successfull", data: aws.Location, state: State.Success})
+                                fs.rm('dump', {recursive: true}, ()=>{
+                                    fs.rm('restore', {recursive: true}, ()=>{
+                                        res.end()
+                                    })
+                                })
+                                }
+                            })
+                        }
+                    })
+                } catch (e) {
+                    await axios.post(`${baseUrl}/logger`, {id, message: e.message, data: '', state: State.Failed})
+                    res.end()
+                }
+    
+            }
+        })
+    }
 
-        }
-    })
 }
 
 export {createDump as createDumpHandler}
