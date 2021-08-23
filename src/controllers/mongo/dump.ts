070cd808b8ce14b2007313bfeb4e96ef35a3965c
RUPAK <58807337+RUPAAK@users.noreply.github.com>
2021-08-23 22:49:42 +0545

    changes

--- src/controllers/mongo/dump.ts
+++ src/controllers/mongo/dump.ts
@@ -36,11 +36,7 @@ const createDump= async(req: Request, res: Response)=>{
     const s3= new AWS.S3()
 
     const {baseUrl, id, url}= req.body
-    if(!id && !url){
-        // console.log('hi')
-        await axios.post(`${baseUrl}/logger`, {id, message: "Empty Field", data: '', state: State.Failed})
-        res.end()
-    }
+    
     const child= spawn('mongodump', [
         '--gzip',
         `--uri`, `${url}`,
