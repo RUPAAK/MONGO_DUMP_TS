44c28c55a0a637ee6902750a644517e00064f602
rupaak <rupakt525@gmail.com>
2021-08-25 11:14:52 +0545

    restore

--- src/controllers/mongo/restore.ts
+++ src/controllers/mongo/restore.ts
@@ -19,7 +19,6 @@ const createRestore= async(req: Request, res: Response)=>{
         loggerFunction("Empty Field", Restore_State.Restore_Failed, '', baseUrl)
         res.end()
     }else{
-        try {
             const response= await fetch(url)
             const bufferData: Buffer= await response.buffer()
         
@@ -65,10 +64,6 @@ const createRestore= async(req: Request, res: Response)=>{
                     res.end()
                 }
             })
-        } catch (error) {
-            loggerFunction("Restore Failed", Restore_State.Restore_Failed, '', baseUrl)
-            res.end()
-        }
     }
 }
 
